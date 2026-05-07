1e27330cf91fe57217dec621940d4364fd9a17a7
Aman Mangla <114299377+Amangla01@users.noreply.github.com>
2026-05-07 22:52:48 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+    baseURL: "http://13.232.46.81:5000/",
 })
 
 API.interceptors.request.use((req) => {
